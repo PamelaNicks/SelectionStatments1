19bf0f45d4f239fd76797a3adc57ff1ac31a8945
PamelaNicks <pamelanicks@yahoo.com>
2024-08-14 14:37:51 -0500

    exercise 2

--- SelectionStatements2/Program.cs
+++ SelectionStatements2/Program.cs
@@ -4,7 +4,53 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Number Guessing Game
+            /*var r = new Random();
+            var favNumber = r.Next(0, 100);
+
+            Console.WriteLine("Pick a number between 0 - 100");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"{userInput} is too low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine($"{userInput} is too high");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }*/
+
+            //Switch-Case
+            Console.WriteLine("What is your favorite school subject?");
+
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Cool! We use math in daily life.");
+                    break;
+                case "science":
+                    Console.WriteLine("Sweet. Careful with those bubbling beakers.");
+                    break;
+                case "pe":
+                    Console.WriteLine("Fun and sweaty!");
+                        break;
+                case "history":
+                    Console.WriteLine("Ah, yes. The stories are forever to be told.");
+                    break;
+                case "english":
+                    Console.WriteLine("Fabulous!");
+                    break;
+                default:
+                    Console.WriteLine("I don't know that subject.");
+                    break;
+
+            }
         }
     }
 }
--- SelectionStatements2/obj/Debug/net8.0/SelectionStatements2.AssemblyInfo.cs
+++ SelectionStatements2/obj/Debug/net8.0/SelectionStatements2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SelectionStatements2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e27ef1d302714b064c0defe8ab2549ea342759c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SelectionStatements2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SelectionStatements2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
